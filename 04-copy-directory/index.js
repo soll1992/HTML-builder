33f6e0865b1a206786ee5037ef1db2bdcdde7be0
Anton <dr.dud34@gmail.com>
2021-11-05 04:04:22 +0300

    feat: task 4 complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require("path");
+
+const dirPath = path.join(__dirname, "files")
+const copyDirPath = path.join(__dirname, "files-copy")
+
+fs.mkdir(copyDirPath, { recursive: true }, (err) => {
+    if (err) throw err;
+});
+
+fs.readdir(dirPath, function(err, items) {
+  for (const item of items) {
+    const filePath = path.join(dirPath, item)
+    const copyFilePath = path.join(copyDirPath, item)
+    console.log(`${item} скопирован`)
+    fs.copyFile(filePath,copyFilePath, (err) => {
+      if (err) {
+        console.log("Error Found:", err);
+      }});
+  }
+});
+
+
 
