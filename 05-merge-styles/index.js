99cb8b3e265991f1d3e9ae2c8811aff491771ec2
Anton <dr.dud34@gmail.com>
2021-11-09 01:20:42 +0300

    feat: task 5 update promises

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,47 +1,45 @@
 const fs = require('fs');
+const promis = require('fs/promises');
 const path = require("path");
 
 const stylesPath = path.join(__dirname, "styles")
 const bundlePath = path.join(__dirname, "project-dist", "bundle.css")
 
 
-function addCssFile(file) {
-    fs.appendFile(bundlePath, file, (err) => {
-        if (err) throw err;                    
-    });
-}
+async function bundleCss() {
+    await promis.rm(bundlePath, { recursive: true, force: true });
 
-function readCssFile(path) {
-    fs.readFile(path, "utf-8", (err, data) => {
-        if (err) throw err;
-
-        addCssFile(data)
-    });
-} 
-
-function bundle() {
-    fs.readdir(stylesPath, {withFileTypes:true}, function(err, items) {
-        if (err) throw err;
-        for(const item of items) {
-            const itemPath = path.join(stylesPath, item.name)
-            const type = path.extname(item.name);
-            if (type === ".css") {
-                readCssFile(itemPath)
+    function addCssFile(file) {
+      fs.appendFile(bundlePath, file, (err) => {
+          if (err) throw err;                    
+      });
+    }
+  
+    function readCssFile(path) {
+        fs.readFile(path, "utf-8", (err, data) => {
+            if (err) throw err;
+  
+            addCssFile(data)
+        });
+    } 
+  
+    function bundle() {
+        fs.readdir(stylesPath, {withFileTypes:true}, function(err, items) {
+            if (err) throw err;
+            for(const item of items) {
+                const itemPath = path.join(stylesPath, item.name)
+                const type = path.extname(item.name);
+                if (type === ".css") {
+                    readCssFile(itemPath)
+                }
             }
-        }
-    })
+        })
+    }
+  
+    bundle()
 }
 
-function bundleDelete() {
-    fs.unlink(bundlePath, (err) => {
-        if (err) {
-            bundle()
-        };
-    });
-}
-
-bundleDelete()
-bundle()
+bundleCss()
 
 
 
